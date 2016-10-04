95b8cfc4da06b61027c3094acbb51f9e0b28885b
Daniel Schwarz <Daniel Schwarz>
2016-10-04 14:54:34 -0400

    Second commit

--- half_fit.c
+++ half_fit.c
@@ -19,12 +19,7 @@ void * memory_address = &memory_pool;
 
 void *bucket_heads[BUCKET_COUNT];
 
-// indicates whether the buckets are empty or full
-bit_vector_t bit_vector;
-
 void  half_init(void){
-    int i;
-
     // create bit vector that contains whether buckets are empty or not
     bit_vector.buckets = 0;
     // add reserved memory to largest bucket
@@ -36,10 +31,10 @@ void  half_init(void){
  * @param size
  * @return Pointer
  */
-void *half_alloc(unsigned int size){
+void *half_alloc(U32 size){
     // effective size of size+4. We'll be using that from now on
-    unsigned int effective_size = round_up_to_chunk_size(size+4);
-    unsigned int block_size;
+    U32 effective_size = round_up_to_chunk_size(size+4);
+    U32 block_size;
 
     // find bucket
     signed int bucket_index = find_bucket(effective_size);
@@ -53,7 +48,7 @@ void *half_alloc(unsigned int size){
 
     if (first_block_address) {
         // Remove allocated block from its bucket, by modifying the points of its neighbours
-        remove_from_known_bucket(first_block_address, bucket_index);
+        remove_from_known_bucket(first_block_address, (U32)bucket_index);
 
         // split block if >= 32 bytes larger than requested size
         // block size should be in bytes
@@ -62,7 +57,7 @@ void *half_alloc(unsigned int size){
         // todo find out if the smallest block is 32 bytes (4 bytes header, 28 usable). Will this block ever be used?
         if (block_size >= effective_size + CHUNK_SIZE) {
             // create new free block, add to bucket
-            unsigned int new_block_size = block_size - effective_size;
+            U32 new_block_size = block_size - effective_size;
             void * new_block_address = first_block_address + effective_size;
             block_header_t *new_header = (block_header_t *)(new_block_address);
             new_header->block_size = shorten_block_size(new_block_size);
@@ -71,8 +66,11 @@ void *half_alloc(unsigned int size){
             new_header->allocated = 0;
 
             // Add new block to appropriate bucket
-            unsigned int new_bucket_index = get_bucket_index(new_block_size);
-            add_to_known_bucket(new_block_address, new_bucket_index);
+            S32 new_bucket_index = get_bucket_index(new_block_size);
+            if (new_bucket_index == -1) {
+                printf("Invalid index for new bucket of size %d", new_block_size);
+            }
+            add_to_known_bucket(new_block_address, (U32)new_bucket_index);
 
 
             // Change the size of this header
@@ -91,17 +89,56 @@ void *half_alloc(unsigned int size){
 }
 
 void  half_free(void * address){
-    // Merge with either/both of neighbouring blocks if they are free
+    // free the block at the given address
+    // create a new block from the adjacent blocks, if they are unallocated
+    void * effective_address = address - HEADER_SIZE;
+    block_header_t * header = (block_header_t *)(effective_address);
+    // pointer to the location of the new header
+    block_header_t * new_header = header;
+
+    unsigned int new_block_size = expand_block_size(header->block_size);
+    block_header_t * previous_block = (block_header_t *)expand_address(header->previous_block, (U32)effective_address);
+    block_header_t * next_block = (block_header_t *)expand_address(header->next_block, (U32)effective_address);
+    // pointer to the block after the new block
+    block_header_t * new_next_block = next_block;
+
+    if (next_block && !next_block->allocated) {
+        new_block_size += expand_block_size(next_block->block_size);
+        new_next_block = (block_header_t *)expand_address(next_block->next_block, (U32)next_block);
+
+        S32 next_bucket_index = get_bucket_index(next_block->block_size);
+        // todo handle bucket_index = -1
+        remove_from_known_bucket(next_block, (U32)next_bucket_index);
+    }
+    if (previous_block && !previous_block->allocated) {
+        new_block_size += expand_block_size(previous_block->block_size);
+        new_header = previous_block;
+        S32 previous_bucket_index = get_bucket_index(previous_block->block_size);
+        remove_from_known_bucket(previous_block, (U32)previous_bucket_index);
+    }
+
+    new_header->block_size = shorten_block_size(new_block_size);
+    new_header->allocated = 0;
+
+    if (new_next_block) {
+        new_header->next_block = shorten_address(new_next_block);
+    } else {
+        new_header->next_block = shorten_address(new_header);
+    }
+
     // add block to appropriate bucket
+    S32 new_block_bucket = get_bucket_index(new_block_size);
+    // todo handle -1
+    add_to_known_bucket(new_header, (U32)new_block_bucket);
 }
 
 /**
  * Remove the given, currently unused block from the given bucket
  */
-void remove_from_known_bucket(void * block_address, void * bucket_index) {
+void remove_from_known_bucket(void * block_address, U32 bucket_index) {
     unused_block_header_t *header = (unused_block_header_t*)(block_address+HEADER_SIZE);
 
-    void * next_in_bucket_pointer = expand_address(header->next_block, block_address);
+    void * next_in_bucket_pointer = expand_address(header->next_block, (U32)block_address);
 
     bucket_heads[bucket_index] = next_in_bucket_pointer;
 
@@ -113,8 +150,8 @@ void remove_from_known_bucket(void * block_address, void * bucket_index) {
     }
 }
 
-void add_to_known_bucket(void * address, unsigned int bucket_index) {
-    unsigned int short_address = shorten_address(address);
+void add_to_known_bucket(void * address, U32 bucket_index) {
+    U32 short_address = shorten_address(address);
     unused_block_header_t *this_header = (unused_block_header_t*)(address+HEADER_SIZE);
 
     // updates pointers in header
@@ -142,13 +179,19 @@ void add_to_known_bucket(void * address, unsigned int bucket_index) {
  * @param size
  * @return
  */
-signed int find_bucket(unsigned int size) {
+signed int find_bucket(U32 size) {
     signed int guaranteed_index = get_guaranteed_bucket(size);
 
     if (guaranteed_index == -1) {
         return guaranteed_index;
     } else {
-        
+        while((bit_vector.buckets & (1 << guaranteed_index)) == 0) {
+            guaranteed_index++;
+            if (guaranteed_index >= BUCKET_COUNT) {
+                return -1; // early return here, to stop an out of bounds exception later
+            }
+        }
+        return guaranteed_index;
     }
 }
 
@@ -157,7 +200,7 @@ signed int find_bucket(unsigned int size) {
  * @param size Size in bytes
  * @return index of the corresponding bucket. -1 if no bucket exists
  */
-signed int get_bucket_index(unsigned int size) {
+signed int get_bucket_index(U32 size) {
     if (size > MAX_SIZE) {
         return -1;
     }
@@ -166,18 +209,20 @@ signed int get_bucket_index(unsigned int size) {
     // value is initially the number of 32 byte chunks that fit inside size
     int value = size >> CHUNK_SIZE_POWER; // shift it right 5
 
+    // keep dividing by two until the value is 1. If the value is 0, never enter the while loop
     while (value > 1) {
         value = value >> 1;
         bucket_index++;
     }
     return bucket_index;
 }
+
 /**
  * Get the index for the bucket that will definitely fit the given size
  * @param size Size in bytes
  * @return index of the corresponding bucket. -1 if no bucket exists
  */
-signed int get_guaranteed_bucket(unsigned int size) {
+signed int get_guaranteed_bucket(U32 size) {
     if (size > MAX_SIZE) {
         return -1;
     }
@@ -194,22 +239,24 @@ signed int get_guaranteed_bucket(unsigned int size) {
 
     int bucket_index = 0;
     // value is initially the number of 32 byte chunks that fit inside size
-    int value = size >> CHUNK_SIZE_POWER; // shift it right 5
+    int value = size >> CHUNK_SIZE_POWER; // shift it right 5, i.e. divide it by 32
     int has_remainder = 0; // true if size is not a power of 2
 
     if ((size & 31) && value==1) {
-    has_remainder=1; // value is in 33-63 range
+        has_remainder=1; // value is in 33-63 range
     }
 
+    // keep dividing the value by two until it equals 1
     while (value > 1) {
-    if ((size & 1) != 0) {
-    has_remainder = 1;
-    }
-    value = value >> 1;
-    bucket_index++;
+        if ((size & 1) != 0) {
+            has_remainder = 1;
+        }
+        value = value >> 1;
+        bucket_index++;
     }
     if (has_remainder) {
-    bucket_index += 1;
+        // if there was a remainder, then we need the next bucket up to get a guaranteed fit
+        bucket_index += 1;
     }
     return bucket_index;
 }
@@ -219,25 +266,26 @@ signed int get_guaranteed_bucket(unsigned int size) {
  * Requires the 'null pointer' value be provided. The null_pointer_value is the memory location of the header
  * the pointer is being read from
  */
-void * expand_address(unsigned int short_address, unsigned int null_pointer_value) {
+void * expand_address(U32 short_address, U32 null_pointer_value) {
     void * address = (short_address << CHUNK_SIZE_BITS) + memory_address;
-    if (address == null_pointer_value) {
+    if ((int)address == null_pointer_value) {
         return NULL;
     } else {
         return address;
     }
 }
 
-unsigned int shorten_address(void * address) {
+
+U32 shorten_address(void *address) {
     if (address < memory_address) {
-        printf("ERROR: address is out of bounds");
+        printf("ERROR: address is out of bounds\n");
     }
     return (address - memory_address) >> (CHUNK_SIZE_BITS);
 }
 
-unsigned int round_up_to_chunk_size(unsigned int value) {
-    unsigned int remainder = value & CHUNK_SIZE;
-    unsigned int scaled = value >> CHUNK_SIZE_POWER;
+U32 round_up_to_chunk_size(U32 value) {
+    U32 remainder = value & CHUNK_SIZE;
+    U32 scaled = value >> CHUNK_SIZE_POWER;
     if (remainder > 0) {
         scaled += 1;
     }
@@ -249,7 +297,7 @@ unsigned int round_up_to_chunk_size(unsigned int value) {
  * @param short_size The value stored in the block_size field in the block header
  * @return
  */
-unsigned int expand_block_size(unsigned int short_size) {
+U32 expand_block_size(U32 short_size) {
     return (short_size + 1) << CHUNK_SIZE_POWER; // multiply by 32
 }
 
@@ -257,7 +305,7 @@ unsigned int expand_block_size(unsigned int short_size) {
  * Given a block size in bytes, shortens it. Block size must be a multiple of 32!
  * @return
  */
-unsigned int shorten_block_size(unsigned int size) {
+U32 shorten_block_size(U32 size) {
     if ((size & 31) != 0) {
         printf("ERROR size is not a multiple of 32: %d", size);
     }
--- half_fit.h
+++ half_fit.h
@@ -7,7 +7,9 @@
  *   2.  uWaterloo User ID:  xxxxxxxx@uwaterloo.ca
  */
 
-#define smlst_blk                       5 
+#include "type.h"
+
+#define smlst_blk                       5
 #define smlst_blk_sz  ( 1 << smlst_blk )   // 32
 #define lrgst_blk                       15 
 #define lrgst_blk_sz    ( 1 << lrgst_blk ) // 32768
@@ -16,7 +18,7 @@
 
 struct {
     unsigned int buckets : 11;
-} bit_vector_t;
+} bit_vector;
 
 /**
  * The header in all blocks. Points to the address of the adjacent blocks,
@@ -45,6 +47,21 @@ typedef struct {
 void  half_init( void );
 void *half_alloc( unsigned int );
 void  half_free( void * );
+
+signed int find_bucket(unsigned int size);
+signed int get_bucket_index(unsigned int size);
+signed int get_guaranteed_bucket(unsigned int size);
+
+void remove_from_known_bucket(void * block_address, unsigned int bucket_index);
+void add_to_known_bucket(void * address, unsigned int bucket_index);
+
+unsigned int shorten_address(void * address);
+void * expand_address(unsigned int short_address, unsigned int null_pointer_value);
+
+U32 expand_block_size(U32 short_size);
+U32 shorten_block_size(U32 size);
+
+U32 round_up_to_chunk_size(U32 value);
 
 #endif
 
